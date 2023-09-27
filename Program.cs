515979829a4239bcbb0bc9a2e787b44fc5dfd3f5
Christian Moyes <moyechri846@gmail.com>
2023-09-27 10:58:09 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Dice_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+        }
+    }
+}
