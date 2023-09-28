0883b0e1c9ba2b409e09066387116d6276fed944
Christian Moyes <moyechri846@gmail.com>
2023-09-28 10:53:47 -0400

    In progress

--- Program.cs
+++ Program.cs
@@ -49,9 +49,38 @@
                     choiceNum = 2;
                     Console.WriteLine("You are betting that the dice will not roll doubles");
                 }
+                else if (choice == "EVEN SUM")
+                {
+                    choiceNum = 3;
+                    Console.WriteLine("You are betting that the sum of the dice will be even");
+                }
+                else if (choice == "ODD SUM")
+                {
+                    choiceNum = 4;
+                    Console.WriteLine("You are betting that the sum of the dice will be odd");
+                }
+                else
+                {
+                    Console.WriteLine("How did this happen?");
+                    Environment.Exit(0);
+                }
 
 
-                Console.ReadLine();
+                Console.WriteLine("How much would you like to bet?");
+
+                while (!Double.TryParse(Console.ReadLine(), out bet) || bet < 0)
+                {
+                    Console.WriteLine("Sorry, that's not a valid bet, please try again");
+                }
+
+                Math.Round(bet, 2);
+
+                if (bet > money)
+                {
+                    Console.WriteLine($"I'm assuming you meant to bet all your money, so I'm just going to lock in your bet of {money.ToString("C")}");
+                    bet = money;
+                }
+
             }
         }
     }
